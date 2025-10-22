49c6d5594417215528e257b5c8eb7e599e2165b3
misho-o <mishalfahim360@gmail.com>
2025-10-23 00:08:19 +0500

    Update Task2.cpp

--- Lab5/Task2.cpp
+++ Lab5/Task2.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
+#include <cstdlib>  
+#include <ctime>    
 using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
-};
 
-int lengthTailRec(Node* head, int count = 0) {
-    
-    
-    if (head == NULL)
-        return count;
+void guessNumber(int secret, int currentPlayer, int totalPlayers) {
+    int guess;
+    cout << "Player " << currentPlayer << ", enter your guess (1–100): ";
+    cin >> guess;
+
+    if (guess == secret) {
+        cout << "?? Player " << currentPlayer << " guessed it right! The number was " << secret << "!\n";
+        return; 
+    } 
+    else if (guess < secret) {
+        cout << "Too low!\n";
+    } 
+    else {
+        cout << "Too high!\n";
+    }
 
     
-    return lengthTailRec(head->next, count + 1);
+    int nextPlayer = (currentPlayer % totalPlayers) + 1;
+
+    
+    guessNumber(secret, nextPlayer, totalPlayers);
 }
 
 int main() {
-    
-    Node* head = new Node{10, NULL};
-    head->next = new Node{20, NULL};
-    head->next->next = new Node{30, NULL};
+    srand(time(0)); 
+    int secret = rand() % 100 + 1;
 
-    int len = lengthTailRec(head);
-    cout << "Length of linked list: " << len << endl;
+    int players;
+    cout << "Enter number of players: ";
+    cin >> players;
+
+    cout << "\n--- Number Guessing Game ---\n";
+    guessNumber(secret, 1, players);
 
     return 0;
 }
-
