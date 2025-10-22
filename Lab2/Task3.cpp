d602062a3070fd775167cea3bf2391f8f8b5ac99
misho-o <mishalfahim360@gmail.com>
2025-10-23 01:25:07 +0500

    Update Task3.cpp

--- Lab2/Task3.cpp
+++ Lab2/Task3.cpp
@@ -1,44 +1,48 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Function to check if two people have a common friend
-bool haveCommonFriend(bool friends[5][5], int a, int b) {
-    for (int k = 0; k < 5; ++k) {
-        if (friends[a][k] && friends[b][k]) {
-            return true;  // Found a common friend
+int main() {
+    // Departments and number of courses
+    string departments[] = {"Software Engineering", "Artificial Intelligence", "Computer Science", "Data Science"};
+    int numDepartments = 4;
+    int coursesPerDept[] = {3, 4, 2, 1};
+
+    // Create jagged dynamic array
+    float** GPA = new float*[numDepartments];
+
+    // Allocate memory for each department’s courses
+    for (int i = 0; i < numDepartments; ++i) {
+        GPA[i] = new float[coursesPerDept[i]];
+    }
+
+    // Input GPA for each department
+    cout << "Enter GPAs for each department:\n";
+    for (int i = 0; i < numDepartments; ++i) {
+        cout << "\n" << departments[i] << " (" << coursesPerDept[i] << " courses):\n";
+        for (int j = 0; j < coursesPerDept[i]; ++j) {
+            cout << "  Course " << j + 1 << " GPA: ";
+            cin >> GPA[i][j];
         }
     }
-    return false;  // No common friend found
-}
 
-int main() {
-    // 5x5 Boolean matrix representing friendships
-    bool friends[5][5] = {
-        //  0    1    2    3    4
-        {false, true, false, true, false},  // person 0
-        {true, false, true, false, false},  // person 1
-        {false, true, false, false, false}, // person 2
-        {true, false, false, false, true},  // person 3
-        {false, false, false, true, false}  // person 4
-    };
+    // Display GPAs
+    cout << "\n------------------------------------------\n";
+    cout << "GPAs of Core Courses by Department:\n";
+    cout << "------------------------------------------\n";
 
-    cout << "Friendship Matrix (1 = friends, 0 = not friends):\n";
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j)
-            cout << friends[i][j] << " ";
+    for (int i = 0; i < numDepartments; ++i) {
+        cout << departments[i] << ":\n";
+        for (int j = 0; j < coursesPerDept[i]; ++j) {
+            cout << "  Course " << j + 1 << " GPA = " << GPA[i][j] << endl;
+        }
         cout << endl;
     }
 
-    cout << "\nChecking common friends:\n";
-    cout << "Do 0 and 4 have a common friend? ";
-    cout << (haveCommonFriend(friends, 0, 4) ? "Yes" : "No") << endl;
-
-    cout << "Do 1 and 2 have a common friend? ";
-    cout << (haveCommonFriend(friends, 1, 2) ? "Yes" : "No") << endl;
-
-    cout << "Do 0 and 1 have a common friend? ";
-    cout << (haveCommonFriend(friends, 0, 1) ? "Yes" : "No") << endl;
+    // Deallocate memory
+    for (int i = 0; i < numDepartments; ++i)
+        delete[] GPA[i];
+    delete[] GPA;
 
     return 0;
 }
-
